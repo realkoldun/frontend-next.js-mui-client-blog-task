a744bbc3c6bdb027263523150ed267730508de9e
koldun <anton.kitaev.97@gmail.com>
2025-05-23 16:53:25 +0300

    fix: categories icons type

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -6,28 +6,28 @@ export const categories: CategoriesType[] = [
     {
         id: '1',
         title: Categories.BUSINESS,
-        imgUrl: DEFAULT_ICON_PATH + '/businessCategoryIcon.png',
+        imgUrl: DEFAULT_ICON_PATH + '/businessCategoryIcon.svg',
         description:
             'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
     },
     {
         id: '2',
         title: Categories.STARTUP,
-        imgUrl: DEFAULT_ICON_PATH + '/startupCategoryIcon.png',
+        imgUrl: DEFAULT_ICON_PATH + '/startupCategoryIcon.svg',
         description:
             'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
     },
     {
         id: '3',
         title: Categories.ECONOMY,
-        imgUrl: DEFAULT_ICON_PATH + '/economyCategoryIcon.png',
+        imgUrl: DEFAULT_ICON_PATH + '/economyCategoryIcon.svg',
         description:
             'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
     },
     {
         id: '4',
         title: Categories.TECHNOLOGY,
-        imgUrl: DEFAULT_ICON_PATH + '/technologyCategoryIcon.png',
+        imgUrl: DEFAULT_ICON_PATH + '/technologyCategoryIcon.svg',
         description:
             'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
     },
