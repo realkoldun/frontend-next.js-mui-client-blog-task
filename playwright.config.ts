378517c099a2c8cdf9b639d98bd509aef0dc77a9
koldun <anton.kitaev.97@gmail.com>
2025-06-13 13:07:21 +0300

    fix: removed .env.test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env.test' });
+dotenv.config({ path: './.env.testing' });
 
 export default defineConfig({
     testDir: './e2e',
