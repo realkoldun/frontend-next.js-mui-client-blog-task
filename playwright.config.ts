f4823be7f53ac459e2ef84f70807f2e3bcbb85c2
koldun <anton.kitaev.97@gmail.com>
2025-06-13 13:40:34 +0300

    fix: evn config on deploy

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env.testing' });
+dotenv.config({ path: './.testing' });
 
 export default defineConfig({
     testDir: './e2e',
